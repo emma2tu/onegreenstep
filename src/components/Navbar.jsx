9c34103fbeb89ea2ed66007c2a2e88bd3ee8afe5
Laura <lygg0589@gmail.com>
2025-02-02 09:01:30 -0800

    now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       {/* Desktop Navigation */}
       <ul className="nav-list">
         <li className="brand">One Green Step</li>
-        <li><NavLink to="/" className="nav-link">Challenge</NavLink></li>
+        <li><NavLink to="/" className="nav-link challenge">Challenge</NavLink></li>
         <li><NavLink to="/archive" className="nav-link">Leaf Gallery</NavLink></li>
       </ul>        
     </nav>
