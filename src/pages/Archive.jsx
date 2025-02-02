484d4190433e6f13c5e82c00d7615695cf581542
Annie Gao <anniegao@Annies-MacBook-Air.local>
2025-02-02 03:47:14 -0800

    message

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,35 +1,11 @@
-// blank
 export function Archive ()
 {
-    const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "purple"];
-    var numLeaves = 0;
-
-    function addLeaf()
-    {
-        var leafColor = colors[numLeaves%7];
-        numLeaves++;
-        var yPos = numLeaves*100;
-        var xPos = window.innerWidth * .35;
-        if(numLeaves%2 == 0)
-            xPos = window.innerWidth * .55;
-        return (
-            <>
-            <ellipse cx={xPos} cy={yPos} rx="50" ry="25" fill={leafColor}/>
-            </>
-        )
-    }
 
     return (
         <>
-        <h1>Archive</h1>
-        <p>Welcome to the archive page! See your past accomplishments here!</p>
-        <svg width="auto" height="600">
-            {addLeaf()}
-            {addLeaf()}
-            {addLeaf()}
-            {addLeaf()}
-        </svg>
-        {/*Is this a comment?*/}
+        <h1>Leaf Gallery</h1>
+        <p>Welcome to the leaf gallery! See your past accomplishments here!</p>
+        
         </>
     )
 }
