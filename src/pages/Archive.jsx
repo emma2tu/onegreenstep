ab50703c683c0210edd6148a57a792d41bcc377a
Annie Gao <anniegao@Annies-MacBook-Air.local>
2025-02-02 00:45:10 -0800

    blue circle oops

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -4,7 +4,11 @@ export function Archive ()
     return (
         <>
         <h1>Archive</h1>
-        <p>Welcone to the archive page!</p>
+        <p>Welcone to the archive page! See your past accomplishments here!</p>
+        <svg width="200" height="200">
+            <circle cx="100" cy="100" r="80" fill="blue"/>
+        </svg>
         </>
+        //figure out how to put shapes on the screen
     )
 }
