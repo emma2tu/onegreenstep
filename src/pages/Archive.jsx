9f62cfa7b46951b5777b7be8cdb49903bf0d6873
Annie Gao <anniegao@Annies-MacBook-Air.local>
2025-02-02 04:09:25 -0800

    leaf gallery worked on

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,11 +1,25 @@
 export function Archive ()
 {
+    const imageURLs = [
+        "1.png",
+        "2.png",
+        "3.png",
+        "4.png"
+    ];
+
+    //when doing it fr, start with imageURLs being empty, then imageURLs.push() the image link when the complete button is clicked
+    //still need to figure out how to iterate through the array and add each article to the section
 
     return (
         <>
         <h1>Leaf Gallery</h1>
         <p>Welcome to the leaf gallery! See your past accomplishments here!</p>
-        
+        <section class="leaves">
+            <article><img class="image" src={imageURLs[0]}></img></article>
+            <article><img class="image" src={imageURLs[1]}></img></article>
+            <article><img class="image" src={imageURLs[2]}></img></article>
+            <article><img class="image" src={imageURLs[3]}></img></article>
+        </section>
         </>
     )
 }
