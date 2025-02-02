7f60c637f9e23162b30bca2f63b0f8c606c46d8d
Annie Gao <anniegao@Annies-MacBook-Air.local>
2025-02-02 02:25:03 -0800

    some work with shapes

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,14 +1,35 @@
 // blank
 export function Archive ()
 {
+    const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "purple"];
+    var numLeaves = 0;
+
+    function addLeaf()
+    {
+        var leafColor = colors[numLeaves%7];
+        numLeaves++;
+        var yPos = numLeaves*100;
+        var xPos = window.innerWidth * .35;
+        if(numLeaves%2 == 0)
+            xPos = window.innerWidth * .55;
+        return (
+            <>
+            <ellipse cx={xPos} cy={yPos} rx="50" ry="25" fill={leafColor}/>
+            </>
+        )
+    }
+
     return (
         <>
         <h1>Archive</h1>
-        <p>Welcone to the archive page! See your past accomplishments here!</p>
-        <svg width="200" height="200">
-            <circle cx="100" cy="100" r="80" fill="blue"/>
+        <p>Welcome to the archive page! See your past accomplishments here!</p>
+        <svg width="auto" height="600">
+            {addLeaf()}
+            {addLeaf()}
+            {addLeaf()}
+            {addLeaf()}
         </svg>
+        {/*Is this a comment?*/}
         </>
-        //figure out how to put shapes on the screen
     )
 }
