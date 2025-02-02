0d794d49e7bd4684d1c3c571d5049fc4f6d3f96f
Annie Gao <anniegao@Annies-MacBook-Air.local>
2025-02-02 10:39:00 -0800

    back to grey images

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -12,12 +12,12 @@ export function Archive ()
     ];
 
     var printImageURLs = [
-        "1.png",
-        "2.png",
+        "7.png",
+        "8.png",
         "9.png",
-        "5.png",
+        "10.png",
         "11.png",
-        "6.png"
+        "12.png"
     ];
 
     return (
